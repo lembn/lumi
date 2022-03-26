3b703fa7d3eb49a5db7b04d88e98957e88df9993
kunal-charn <charnkunal@gmail.com>
2022-03-26 10:18:47 +0000

    "test"

---
+++ src/main/java/com/redg/lumi/JacksonObjectMapper.java
@@ -0,0 +1,24 @@
+package com.redg.lumi;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+public class JacksonObjectMapper {
+    ObjectMapper mapper = new ObjectMapper();
+    public JacksonObjectMapper(){
+
+    }
+
+
+
+}
+
+
+
