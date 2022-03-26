33dc2c75211d9c75ac849d8a434d6e41243a7bcb
kunal-charn <charnkunal@gmail.com>
2022-03-26 09:59:02 +0000

    "test"

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.redg.lumi {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
 
 
     opens com.redg.lumi to javafx.fxml;
