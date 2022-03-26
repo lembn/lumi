0a5eb24dcb4c32a458128da7f9271e92fd128389
Narges <nrahm77@gmail.com>
2022-03-26 12:37:27 +0000

    Ground data constructor made

--- src/main/java/com/redg/lumi/dataProcessing/GroundStation.java
+++ src/main/java/com/redg/lumi/dataProcessing/GroundStation.java
@@ -1,6 +1,20 @@
 package com.redg.lumi.dataProcessing;
 
+import java.util.List;
+
 public class GroundStation extends SimulationChild{
     private int stationID;
+    private double longitude;
+    private double latitude;
+    private double altitude;
+    private List<SatallitePass> stationReadings;
+
     //private ArrayList<Pass> passes;
+    public GroundStation(int givenID, double givenLon, double givenLat, double givenAlt, List<SatallitePass> givenReadings){
+        this.stationID = givenID;
+        this.longitude = givenLon;
+        this.latitude = givenLat;
+        this.altitude = givenAlt;
+        this.stationReadings = givenReadings;
+    }
 }
