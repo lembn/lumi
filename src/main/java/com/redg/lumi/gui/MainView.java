a9f602d928273011e35fe5cc74c7fb183fbc5568
lembn <61047330+lembn@users.noreply.github.com>
2022-03-26 16:29:50 +0000

    write inital ui

--- src/main/java/com/redg/lumi/gui/MainView.java
+++ src/main/java/com/redg/lumi/gui/MainView.java
@@ -1,14 +1,27 @@
 package com.redg.lumi.gui;
 
+import com.redg.lumi.gui.ui.UI;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class MainView extends Stage {
-    public MainView() {
-        BorderPane root = new BorderPane();
+    private static final int ROOT_PAD = 10;
 
-        Scene scene = new Scene(root, 800, 600);
+    public MainView() {
+        StackPane root = new StackPane();
+        root.setAlignment(Pos.CENTER_LEFT);
+        root.setPadding(new Insets(ROOT_PAD, ROOT_PAD, ROOT_PAD, ROOT_PAD));
+
+        root.getChildren().addAll(new UI());
+        root.requestFocus();
+
+        Scene scene = new Scene(root, 1600, 850);
+        scene.getStylesheets().add(getClass().getResource("styles.css").toString());
 
         this.setScene(scene);
         this.setTitle("Lumi");
